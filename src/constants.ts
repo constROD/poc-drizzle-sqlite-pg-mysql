85cb44d39d9a54a2bc0e919e7e8c48e5e32df1ff
constROD <rialcantara21@gmail.com>
2023-10-28 04:54:53 +0800

    chore: update env

---
+++ src/constants.ts
@@ -0,0 +1,9 @@
+export const STAGES = {
+  DEV: 'dev',
+  PROD: 'prod',
+  TEST: 'test',
+} as const;
+
+export type Stage = (typeof STAGES)[keyof typeof STAGES];
+
+export const STAGE_VALUES = Object.values(STAGES);
--- src/env.ts
+++ src/env.ts
@@ -1,20 +1,28 @@
-import 'dotenv/config';
+import { config } from 'dotenv';
 import { z } from 'zod';
+import { STAGES } from './constants';
+
+const initEnvConfig = () => {
+  if (process.env.NODE_ENV === 'test') return config({ path: '.env.test' });
+  return config();
+};
+
+initEnvConfig();
 
 const envSchema = z.object({
+  STAGE: z.enum([STAGES.DEV, STAGES.PROD, STAGES.TEST]).default(STAGES.DEV),
   DB_NAME: z.string(),
   DB_USER: z.string(),
   DB_PASSWORD: z.string(),
   DB_HOST: z.string(),
-  DB_PG_PORT: z.coerce.number(),
-  DB_MYSQL_PORT: z.coerce.number(),
+  DB_PORT: z.coerce.number(),
 });
 
 export const envConfig = envSchema.parse({
+  STAGE: process.env.STAGE,
   DB_NAME: process.env.DB_NAME,
   DB_USER: process.env.DB_USER,
   DB_PASSWORD: process.env.DB_PASSWORD,
   DB_HOST: process.env.DB_HOST,
-  DB_PG_PORT: process.env.DB_PG_PORT,
-  DB_MYSQL_PORT: process.env.DB_MYSQL_PORT,
+  DB_PORT: process.env.DB_PORT,
 });
